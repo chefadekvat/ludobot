26a79c895c4ab49033c78e719434c82d6c252e43
Chef <chefadekvat@gmail.com>
2026-01-24 02:01:25 +0300

    add env vars

---
+++ setup.sh
@@ -0,0 +1,9 @@
+export PROJECT_ROOT=$(pwd)
+export ENV_ROOT=${PROJECT_ROOT}/env
+
+# tools
+export SQLGEN_ROOT=${PROJECT_ROOT}/tools/sqlgen
+
+# services
+export LUDOBOT_ROOT=${PROJECT_ROOT}/services/ludobot
+export USER_TRAITS_ROOT=${PROJECT_ROOT}/services/user-traits
